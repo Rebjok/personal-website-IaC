1cec0003eac23c9984f5e58c8cb032cfde623d50
Rebi.boy <rebjok911@gmail.com>
2023-11-04 18:57:33 +0000

    created IAM role for Lambda function

--- infra/main.tf
+++ infra/main.tf
@@ -1,3 +1,4 @@
+## Lambda function
 resource "aws_lambda_function" "personal-website-func" {
   filename         = data.archive_file.zip_the_python_code.output_path
   source_code_hash = data.archive_file.zip_the_python_code.output_base64sha256
@@ -5,4 +6,25 @@ resource "aws_lambda_function" "personal-website-func" {
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = "func.handler"
   runtime          = "python3.11"
+}
+
+##IAM Role for lambda Function
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
 }
